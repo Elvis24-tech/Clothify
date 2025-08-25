786a49f951f8968d33d8f084e88ed54953d647df
elvis <kariuki.elvis@student.moringaschool.com>
2025-08-25 23:24:15 +0300

    mans' page

--- src/data/men.js
+++ src/data/men.js
@@ -3,36 +3,36 @@ export const menProducts = [
       id: 1,
       name: "Casual T-Shirt",
       price: 1200,
-      img: "https://images.unsplash.com/photo-1593032465174-2d2f502a02aa",
+      img: "https://i.ebayimg.com/images/g/aaYAAOSwCA9kAaoy/s-l1200.jpg",
     },
     {
       id: 2,
       name: "Denim Jacket",
       price: 3500,
-      img: "https://images.unsplash.com/photo-1541099649105-f69ad21f3246",
+      img: "https://www.apetogentleman.com/wp-content/uploads/2020/03/wear-denim-jacket-2.jpg",
     },
     {
       id: 3,
       name: "Slim Fit Jeans",
       price: 2800,
-      img: "https://images.unsplash.com/photo-1503342217505-b0a15ec3261c",
+      img: "https://www.crossjeans.com/cdn/shop/files/E_198_026_cross_jeans_UD2024_1_1445x.jpg?v=1714652230",
     },
     {
       id: 4,
       name: "Formal Suit",
       price: 7200,
-      img: "https://images.unsplash.com/photo-1520975918311-cc3e0e7d5b1b",
+      img: "https://i5.walmartimages.com/seo/Wehilion-Mens-Suits-Set-Slim-Fit-Men-3-Piece-Dress-Suit-Prom-Blazer-Wedding-Formal-Jacket-Vest-Pants-Navy-Blue-M_ce590b9b-405f-4948-af6a-a817cd66f9cd.4e404934c089dc5fabeb4616f32245e6.jpeg",
     },
     {
       id: 5,
       name: "Sneakers",
       price: 3000,
-      img: "https://images.unsplash.com/photo-1528701800489-20be9c5c7d4c",
+      img: "https://cdnimg.brunomarc.com/brunomarcs/image/article/20220818_79/NY-03-mens-post-sneakers-with-jeans2.jpg"
     },
     {
       id: 6,
       name: "Hoodie",
       price: 2600,
-      img: "https://images.unsplash.com/photo-1602810317740-9cc1e9efb6e1",
+      img: "https://m.media-amazon.com/images/I/71mhs8nkGeL._UY1000_.jpg",
     },
   ];
