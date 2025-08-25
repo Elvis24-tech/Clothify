ad054d304bdf7c6ed8594df61fd7b730f73add8a
elvis <kariuki.elvis@student.moringaschool.com>
2025-08-25 23:11:39 +0300

    women page

--- src/data/men.js
+++ src/data/men.js
@@ -17,5 +17,22 @@ export const menProducts = [
       price: 2800,
       img: "https://images.unsplash.com/photo-1503342217505-b0a15ec3261c",
     },
+    {
+      id: 4,
+      name: "Formal Suit",
+      price: 7200,
+      img: "https://images.unsplash.com/photo-1520975918311-cc3e0e7d5b1b",
+    },
+    {
+      id: 5,
+      name: "Sneakers",
+      price: 3000,
+      img: "https://images.unsplash.com/photo-1528701800489-20be9c5c7d4c",
+    },
+    {
+      id: 6,
+      name: "Hoodie",
+      price: 2600,
+      img: "https://images.unsplash.com/photo-1602810317740-9cc1e9efb6e1",
+    },
   ];
-  
--- src/data/women.js
+++ src/data/women.js
@@ -3,19 +3,36 @@ export const womenProducts = [
       id: 1,
       name: "Summer Dress",
       price: 2500,
-      img: "https://images.unsplash.com/photo-1520974749544-5f18f3cc2bb0",
+      img: "https://i5.walmartimages.com/asr/572c7130-580b-45f5-ac0c-bb8e7b3b6f98.6e5b7bd7bafc515d3891e3bd3348dff2.jpeg?odnHeight=768&odnWidth=768&odnBg=FFFFFF",
     },
     {
       id: 2,
       name: "Leather Jacket",
       price: 4800,
-      img: "https://images.unsplash.com/photo-1533738363-b7f9aef128ce",
+      img: "https://www.merlinbikegear.com/wp-content/uploads/2023/03/Bristol-Ladies-Jacket-Brown-Zipping-Up.jpg",
     },
     {
       id: 3,
       name: "Elegant Blouse",
       price: 2200,
-      img: "https://images.unsplash.com/photo-1551861568-b739c11f2ab4",
+      img: "https://i5.walmartimages.com/asr/7f3cb269-6563-4f41-9cd3-62bd67e83869.2ca1bfdb453664e30d9411356f99e142.jpeg",
+    },
+    {
+      id: 4,
+      name: "Floral Skirt",
+      price: 2000,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQJfyRHyx7-xoygzJAJ7xMxa2tuHwqhZgZ-Q&s",
+    },
+    {
+      id: 5,
+      name: "High Heels",
+      price: 3200,
+      img: "https://tabasamubridals.co.ke/wp-content/uploads/2024/06/BridalShoesHighHeelsTabasamuBridals-scaled.jpg",
+    },
+    {
+      id: 6,
+      name: "Handbag",
+      price: 4500,
+      img: "https://www.purpink.co.ke/cdn/shop/articles/Blog-Bag-Banner.jpg?v=1693573362",
     },
   ];
-  
